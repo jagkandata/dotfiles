8b21ecef3d0c0fb25bfb1704aba0a54d3847d640
micke <micke@axevalla.fhsk.se>
2020-09-05 16:27:45 +0200

    symlinks instead of copying

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-cp -R config ~/.config
-cp Xresources ~/.Xresources
-cp xinitrc ~/.xinitrc
+ln -sf config/dunst ~/.config/dunst
+ln -sf config/i3 ~/.config/i3
+ln -sf config/picom.conf ~/.config/picom.conf
+ln -sf config/polybar ~/.config/polybar
+ln -sf config/rofi ~/.config/rofi
+ln -sf Xresources ~/.Xresources
+ln -sf xinitrc ~/.xinitrc
 
 yay -S - < packages.txt
