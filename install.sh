8c3e032f11dfa6b4f8df14317e051e1b0a2f6364
micke <micke@axevalla.fhsk.se>
2020-09-12 09:42:31 +0200

    fixed gpg-keys to allow for spotify installation

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ ln -sf $DIR/config/polybar ~/.config/polybar
 ln -sf $DIR/config/rofi ~/.config/rofi
 ln -sf $DIR/Xresources ~/.Xresources
 ln -sf $DIR/xinitrc ~/.xinitrc
+ln -sf $DIR/gnupg ~/.gnupg
 
 cd $DIR/vim && sh ./viminstall.sh
 
